321132de7f0cfaac7d8b2d13d82c67ef10757af1
Shawn Wang <yuan.hsiang.wang@gmail.com>
2017-07-25 22:48:50 -0600

    Shawn 2017/0725

---
+++ app/src/main/java/com/shawn/ideexplorer/MainActivity.java
@@ -0,0 +1,13 @@
+package com.shawn.ideexplorer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
